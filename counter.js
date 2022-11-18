de54579333ced51cd15285f4afc7f3ae3bec1d52
TheCodister <quang.nguyen26012907@hcmut.edu.vn>
2022-11-18 22:14:24 +0700

    Nope

--- counter.js
+++
@@ -1,8 +0,0 @@
-const plus = document.querySelector(".num");
-let a = 1;
-plus.addEventListener("click", () => {
-  a++;
-  a = (a < 10) ? "0" + a : a;
-  plus.innerText = a;
-  console.log(a);
-});
