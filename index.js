dbd4e0bcfbcd4e15facd6687229175518dabc917
TheCodister <quang.nguyen26012907@hcmut.edu.vn>
2022-11-20 16:32:34 +0700

    Update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const button = document.querySelector(".heart-like-button"), num = document.querySelector(".counter");
-let a = 1;
+let a = 0;
 button.addEventListener("click", () => {
   if (button.classList.contains("liked")) {
     button.classList.remove("liked");
@@ -10,4 +10,16 @@ button.addEventListener("click", () => {
   a = (a < 10) ? "0" + a : a;
   num.innerText = a;
   console.log(a);
+  if(a === 100){
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+    alert("Congratulation! You have unlock hidden achievement!!!");
+  }
 });
+
+function totalclick(click){
+  if(click === 0){
+    a = 0;
+    a = (a < 10) ? "0" + a : a;
+    num.innerText = a;
+  }
+}
